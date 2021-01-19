d1cb161764a360d9ab4087e368a43610e2fe1916
priyankavuppuganti <62930525+priyankavuppuganti@users.noreply.github.com>
2021-01-19 14:23:05 +0530

    Create MainActivity.java

---
+++ Program-1a/MainActivity.java
@@ -0,0 +1,21 @@
+package com.e.a1stprogram;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
